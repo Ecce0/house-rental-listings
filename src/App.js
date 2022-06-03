e638c455081fb8ff8b26f9ce65f9c401a99cbb95
E. Collier <ecollier871@outlook.com>
2022-06-02 20:51:41 -0400

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Explore from './pages/Explore'
+import Offer from './pages/Offer'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Explore />} />
+        <Route path='/offers' element={<Offer />} />
+        <Route path='/profile' element={<SignIn />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+      </Routes>
+      <Navbar />
+    </Router>
+    </>
   );
 }
 
