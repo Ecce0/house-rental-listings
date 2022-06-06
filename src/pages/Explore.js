c0bd798ad4e5ce255e0485de1fb194cab17f4d5a
E. Collier <ecollier871@outlook.com>
2022-06-06 17:14:25 -0400

    categories page added

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React from "react"
+import { Link } from 'react-router-dom'
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
 const Explore = () => {
-  return <div>Explore</div>;
-};
+  
+  
+  return(
+    <div className="explore">
+      <header>
+        <p className="pageHeader">
+          Explorer
+        </p>
+      </header>
+      <main>
+        {/*slider*/}
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to='/category/rent'>
+            <img src={rentCategoryImage} alt="rent" className='exploreCategoryImg' />
+            <p className="exploreCategoryName">Places for Rent</p>
+          </Link>
+          <Link to='/category/sell'>
+            <img src={sellCategoryImage} alt="sell" className='exploreCategoryImg' />
+            <p className="exploreCategoryName">Places for Sale</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  )
+  
+}
 
-export default Explore;
+export default Explore
