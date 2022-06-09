2fcbfee5265a819a5239ae3c61eaec7c0058e7c9
E. Collier <ecollier871@outlook.com>
2022-06-09 17:36:47 -0400

    tailwind and daisyui added for styling edits

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		styled: true,
+		themes: ['coffee'],
+		base: true,
+		utils: true,
+		logs: true,
+		rtl: false,
+		prefix: '',
+	},
+	themes: {
+		mytheme: {
+			info: '#8CCAC1',
+
+			success: '#9CB686',
+
+			warning: '#FFD261',
+
+			error: '#FC9783',
+		},
+	},
+}
